1a2ef995e060010086bb012522c5751a41d57845
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-05-06 19:54:49 -0700

    transformed the parent class Sorter to an abstract class

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -4,14 +4,16 @@ interface Sortable {
   swap(index1: number, index2: number): void;
 }
 
-export class Sorter {
-  constructor(public collection: Sortable) {}
+export abstract class Sorter {
+  abstract compare(index1: number, index2: number): boolean;
+  abstract swap(index1: number, index2: number): void;
+  abstract length: number;
 
   sort(): void {
-    for (let i = this.collection.length - 1; i >= 0; i--) {
+    for (let i = this.length - 1; i >= 0; i--) {
       for (let j = 0; j < i; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
